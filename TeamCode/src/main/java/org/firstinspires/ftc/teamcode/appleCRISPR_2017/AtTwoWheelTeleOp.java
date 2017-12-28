f29586dd3860d845446dbe9b9d33e4013edcfdea
HazilTheNut <hurin615@gmail.com>
2017-12-27 20:56:25 -0600

    Repackaging

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/appleCRISPR_2017/AtTwoWheelTeleOp.java
@@ -0,0 +1,331 @@
+/* No Copyright (c) 2015 FTC TEAM 7979 APPLE TAU
+
+No rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to none of the limitations in the disclaimer below) 
+provided that none, any, or all of the following conditions are met:
+
+Redistributions of source code must retain the above lack of copyright notice, 
+this list of conditions and the following disclaimer.
+
+Redistributions in binary form mustn't reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of FTC TEAM 7979 APPLE TAU nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE NOT GRANTED BY
+THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYLEFT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYLEFT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; DESTRUCTION CAUSED BY ROUGE ROBOTS; MASSIVE EXPLOSIONS AND COLLAPSE
+OF SOCIETY; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WEATHERING CONTRACT, STRICT LIABILITY,
+OR TORTOISE (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package org.firstinspires.ftc.teamcode.appleCRISPR_2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.appleCRISPR_2017.AtRevComponents.*;
+
+/**
+ * TeleOp Mode
+ * <p/>
+ * Enables control of the robot via the gamepad
+ */
+@TeleOp(name = "MOTOR TEST", group = "Concept")
+public class AtTwoWheelTeleOp extends OpMode {
+    //AtWheelPair driveWheels;
+    //AtAttachment attachments;
+    private int wingTime = 0;
+    private boolean slow = true;
+    private boolean linked = false;
+    private boolean justLinked = false;
+    private boolean justWinged = false;
+    private boolean wingsUp = false;
+    private boolean switchedSlowLastTime = false;
+    private final int wingWaitTime = 300;
+    float left;
+    float right;
+
+    String[] bads = {" ", " ", " ", " "};
+
+    private final float slowModeMultiplier = .4f;
+    private final boolean wingInactive = false;
+
+    private AtREVMotor spinner1;
+    private AtREVMotor spinner2;
+    private AtREVMotor beltMover;
+    private AtREVMotor motor1;
+    private AtREVModule revModule;
+
+    private AtREVServo servo1;
+    private AtREVServo servo2;
+
+    private float servo1Dir = 0.5f;
+    private boolean servo1DirPressed = false;
+    private float servo2Dir = 0.5f;
+    private boolean servo2DirPressed = false;
+
+    /**
+     * Constructor
+     */
+    public AtTwoWheelTeleOp() {
+
+    }
+
+    /*
+     * Code to run when the op mode is first enabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+     */
+    @Override
+    public void init() {
+		/*
+         * Use the hardwareMap to get the dc motors and servos by name. Note
+		 * that the names of the devices must match the names used when you
+		 * configured your robot and created the configuration file.
+		 */
+
+		revModule = new AtREVModule();
+
+        /*
+        spinner1 = new AtREVMotor("belt_L");
+        revModule.add(spinner1);
+
+        spinner2 = new AtREVMotor("belt_R");
+        revModule.add(spinner2);
+
+        beltMover = new AtREVMotor("belt_mover");
+        revModule.add(beltMover);
+        */
+
+        //servo1 = (AtREVServo)revModule.add(new AtREVServo("servo1"));
+        //servo1.setPosition(1.5);
+        //servo2 = (AtREVServo)revModule.add(new AtREVServo("servo2"));
+        //servo2.setPosition(1.5);
+
+        motor1 = (AtREVMotor)revModule.add(new AtREVMotor("motor1"));
+
+        telemetry.addData("Initialization Success: ", revModule.initialize(hardwareMap));
+        telemetry.update();
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+     */
+    @Override
+    public void loop() {
+
+        ///////////////  D R I V E R   1  ///////////////
+        driver1();
+
+        ///////////////  D R I V E R   2  ///////////////
+        //driver2();
+
+        /////////////  T E L E M E T R Y  ///////////////
+        telemetry();
+
+    }
+
+    //Controls and gizmos for the first driver (chassis)
+    public void driver1() {
+        //Spinner Value
+        float spin1 = (float)scaleInput(gamepad1.right_stick_y);
+        //float spin2 = (float)scaleInput(gamepad1.left_stick_y);
+        //float mover = (float)scaleInput(gamepad1.right_stick_x);
+
+        if (gamepad1.a){
+            telemetry.addLine("a pressed");
+        } else telemetry.addLine("a not pressed");
+
+        /*if (spin1 != 0){
+            telemetry.addLine("Joystick working");
+        } else telemetry.addLine("Joystick not working");
+        */
+        //if (spin1 < 0.95f) spin1 /= 3;
+        //if (spin2 < 0.95f) spin2 /= 3; //For powering arm
+        //if (mover < 0.95f) mover /= 3;
+
+        //((AtREVMotor)revModule.get("motor1")).setPower(spin1);
+
+
+
+        /*if (gamepad1.a){ //Unison Spinning +
+            ((AtREVMotor)revModule.get("belt_L")).setPower(.5f);
+            ((AtREVMotor)revModule.get("belt_R")).setPower(-.5f);
+        } else if (gamepad1.y){ //Unison Spinning -
+            ((AtREVMotor)revModule.get("belt_L")).setPower(.5f);
+            ((AtREVMotor)revModule.get("belt_R")).setPower(-.5f);
+        } else if (gamepad1.x){ //belt moving +
+            ((AtREVMotor)revModule.get("belt_mover")).setPower(.5f);
+        } else if (gamepad1.b){ //belt moving -
+            ((AtREVMotor)revModule.get("belt_mover")).setPower(-.5f);
+        } else {
+            ((AtREVMotor)revModule.get("belt_L")).setPower(spin1);
+            ((AtREVMotor)revModule.get("belt_R")).setPower(spin2);
+            ((AtREVMotor)revModule.get("belt_mover")).setPower(mover);
+        }
+        */
+        /*
+        if (gamepad1.dpad_left && !servo1DirPressed) {
+            servo1Dir *= -1;
+            servo1DirPressed = true;
+        } else if (!gamepad1.dpad_left){
+            servo1DirPressed = false;
+        }
+        if (gamepad1.dpad_right && !servo2DirPressed) {
+            servo2Dir *= -1;
+            servo2DirPressed = true;
+        } else if (!gamepad1.dpad_right){
+            servo2DirPressed = false;
+        }
+
+        double servoMotionlessPos = 0.5;
+
+        if (gamepad1.left_trigger > 0){
+            servo1.setPosition(servoMotionlessPos + (servo1Dir * gamepad1.left_trigger));
+        } else {
+            servo1.setPosition(servoMotionlessPos - 0.025);
+        }
+        if (gamepad1.right_trigger > 0){
+            servo2.setPosition(servoMotionlessPos + (servo2Dir * gamepad1.right_trigger));
+        } else {
+            servo2.setPosition(servoMotionlessPos);
+        }
+
+        telemetry.addData("Servo1: " + servo1.getPosition() + " dir: " + servo1Dir, "");
+        telemetry.addData("Servo2: " + servo2.getPosition() + " dir: " + servo2Dir, "");
+
+        telemetry.update();
+        */
+    }
+
+
+    //Controls and gizmos for the second driver (attachments)
+    public void driver2() {
+
+        float leftA = gamepad2.left_stick_y;
+        float rightA = gamepad2.right_stick_y;
+
+        leftA = (float) scaleInput(leftA) * .6f;
+        rightA = (float) scaleInput(rightA);
+
+        //attachments.moveAiming(leftA);
+        //attachments.moveSpool(rightA);
+
+        if (gamepad2.x) {
+            if (!justWinged) {
+                wingsUp = !wingsUp;
+            }
+            justWinged = true;
+        } else {
+            justWinged = false;
+        }
+       // attachments.setWingsPosition(wingsUp);
+
+        /*
+        if (gamepad2.x && wingTime > wingWaitTime && !wingInactive) { //Simple timer; wingInactive disables entire thing
+            attachments.flapWings();
+            wingTime = 0;
+        }
+        wingTime += 1;
+        */
+    }
+
+
+    //Updates telemetry
+    public void telemetry() {
+        //   attachments.reportPosition(telemetry);
+        //driveWheels.reportPosition(telemetry);
+        /*
+        telemetry.addData("Text", "*** Robot Data***");
+        telemetry.addData("Left  side pwr:", left);
+        telemetry.addData("Left  side raw:", driveWheels.getPower1());
+        telemetry.addData("Right side pwr:", right);
+        telemetry.addData("Right side raw:", driveWheels.getPower2());
+        telemetry.addData("Everything(1): ", gamepad1.toString());
+        telemetry.addData("Everything(2): ", gamepad2.toString());
+        /*  | Code above compresses data into a neat 4 lines |
+        if (driveWheelsBack.frBad) {
+            telemetry.addData("\nFR encoder is bad!\n", "");
+        }
+        if (driveWheelsBack.flBad) {
+            telemetry.addData("\nFL encoder is bad!\n", "");
+        }
+        if (driveWheelsBack.brBad) {
+            telemetry.addData("\nBR encoder is bad!\n", "");
+        }
+        if (driveWheelsBack.blBad) {
+            telemetry.addData("\nBL encoder is bad!\n", "");
+        }
+        */
+    }
+
+
+    /*
+     * Code to run when the op mode is first disabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+     */
+    @Override
+    public void stop() {
+        //driveWheels.stop();
+    }
+
+
+    /*
+     * This method scales the joystick input so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleInput(double dVal) {
+        double[] scaleArray = {0.0, 0.05, 0.06, 0.08, 0.09, 0.11, 0.13, 0.15,
+                0.18, 0.22, 0.25, 0.33, 0.40, 0.52, 0.66, 0.85, 1.00};
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+    /*   An alternative scaler, using logs instead of predefined values
+	double scaleInput(double dVal)  {
+		int sign = (int) (dVal/Math.abs(dVal));  // n / |n| = sign of n (reapply later)
+		dVal = Math.abs(dVal);
+		double raw = Math.pow(10.0,(dVal-1.0)) ; //Natural log
+        dVal = raw -.1 + .1*dVal;
+		return dVal*sign;
+	}
+     */
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/appleCRISPR_2017/AtVVAutonomousRed.java
@@ -0,0 +1,176 @@
+package org.firstinspires.ftc.teamcode.appleCRISPR_2017;
+
+/**
+ *
+ * Created by Jared and Riley on 12-Nov-16.
+ */
+
+//@Autonomous(name = "VV 2 ball Autonomous (Red Side, turning)", group = "Autonomous")
+public class AtVVAutonomousRed extends AtVVAutonomousMethods {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Init
+        ourInit();
+        telemetry.update();
+        // wait for the start button to be pressed
+        telemetry.addData("Hi there!  Press start when you're ready.", "");
+        waitForStart();
+
+        calibrateGyro();
+
+        frontColorSensor.enableLed(true);    // Set the LED in the beginning
+        middleColorSensor.enableLed(true);    // Set the LED in the beginning
+        beaconColorSensor.enableLed(true);
+
+        frontInitialLightReading = getCurrentFrontLightReading();
+        middleInitialLightReading = getCurrentMiddleLightReading();
+
+        /*
+        //TEST OF COLOR SENSOR
+        int dbgt = 0;
+        while (dbgt < 20 * 300){
+            telemetry.addData("MIDDLE LIGHT", getCurrentMiddleLightReading());
+            telemetry.addData("MIDDLE TARGET", middleInitialLightReading);
+            telemetry.addData("FRONT LIGHT", getCurrentFrontLightReading());
+            telemetry.addData("FRONT TARGET", frontInitialLightReading);
+            telemetry.addData("TIMER", System.currentTimeMillis() / 100);
+            dbgt++;
+            Thread.sleep(50);
+            telemetry.addData("Front Sensor Info : ", frontColorSensor.getConnectionInfo());
+            telemetry.addData("Middle Sensor Info: ", middleColorSensor.getConnectionInfo());
+            telemetry.addData("Beacon Sensor Info: ", beaconColorSensor.getConnectionInfo());
+            telemetry.update();
+        }
+        */
+
+        //get out from the wall so we can rotate
+
+        driveAtPower(.6f);
+        telemetry.addData("GRYO VAL: ", gyro.getIntegratedZValue());
+        telemetry.update();
+        Thread.sleep(1100);
+
+        stopDrive();
+
+        //Thread.sleep(300);
+
+        // Rotate a little, like 40ish degrees, so we point toward the white line.
+        setLeftPowers(.4f, .4f);
+        setRightPowers(-.4f, -.4f);
+
+        int t = 0;
+        while (t < 20*MAX_TIME_TO_ROTATE && gyro.getIntegratedZValue() < TURN_ANGLE){
+            telemetry.addData("GRYO VAL: ", gyro.getIntegratedZValue());
+            telemetry.update();
+            Thread.sleep(50); // 20 hertz
+            t++;
+        }
+        if (t > 20*MAX_TIME_TO_ROTATE){ // Just for debug
+            System.out.println("Gyro didn't reach 45 deg in time!!!");
+            telemetry.addData("Uh oh: ", "Gyro didn't reach 45 deg in time!!!");
+            telemetry.update();
+        }
+        stopDrive();
+        //Thread.sleep(500);
+
+        //Throw a ball
+        throwBall();
+        thrower.setPower(0);
+
+        collector.setPower(1f);
+        Thread.sleep(3500);
+        throwBall();
+        thrower.setPower(0);
+        collector.setPower(0);
+
+
+        /*
+        //unturn
+        t = 0;
+        while (t < 20*MAX_TIME_TO_ROTATE && gyro.getIntegratedZValue() > 0){
+            telemetry.addData("GRYO VAL: ", gyro.getIntegratedZValue());
+            telemetry.update();
+            Thread.sleep(50); // 20 hertz
+            t++;
+        }
+        */
+        /*
+
+        // Go forward until sees the line
+        driveAtPower(.7f); //Go fast initially
+        Thread.sleep(1000);
+        driveAtPower(.15f); //Creep Forward for maximum German precision
+        t=0;
+        while (t < 20*MAX_TIME_TO_LINE && !middleSeeingLine()){
+            telemetry.addData("Sensor Read:", getCurrentMiddleLightReading());
+            telemetry.update();
+            Thread.sleep(50); // 20 hertz
+            t++;
+        }
+        if (t > 20*MAX_TIME_TO_LINE){ // Just for debug
+            telemetry.addData("Uh oh: ", "middle sensor didn't see line in time!!!");
+        } else {
+            telemetry.addData("STOPPED DUE TO MIDDLE SENSOR! ", "");
+            telemetry.update();
+        }
+        stopDrive();
+
+        // Creep backwards, cuz we consistently overshoot a bit.
+        driveAtPower(-.15f);
+        Thread.sleep(200);
+        stopDrive();
+
+        telemetry.addData("POST-TURN GRYO VAL: ", preStopVal);
+        //telemetry.addData("FINAL GRYO VAL: ", finalTurnAngle); -- See note on sanity
+        telemetry.update();
+
+        Thread.sleep(500);
+
+        turnTowardBeaconColor();
+        //turnTowardBeaconGyro();
+
+        // Now creep forward towards the beacon.  As we get closer, the color sensor on the pusher
+        //  should read ever-increasing values.  Use this to tell how close we are.
+        driveAtPower(.4f);
+        t=0;
+        while (t < 20*MAX_TIME_TO_APPROACH && !(getCurrentBeaconRedReading()<4 && getCurrentBeaconBlueReading()<4)){
+            telemetry.addData("", "Waiting for beacon color to be big");
+            telemetry.update();
+            Thread.sleep(50); // 20 hertz
+            t++;
+        }
+        stopDrive();
+        telemetry.addData("", "Done approaching beacon");
+        telemetry.addData("Beacon red: ", getCurrentBeaconRedReading());
+        telemetry.addData("Beacon blue: ", getCurrentBeaconBlueReading());
+        telemetry.addData("", "Now pushing button on beacon");
+        telemetry.update();
+
+        if (getCurrentBeaconRedReading() > getCurrentBeaconBlueReading()){
+            pushLeftButton();
+        } else if (getCurrentBeaconRedReading() < getCurrentBeaconBlueReading()){
+            pushRightButton();
+        }
+        // Pause, and then back up a little.
+        Thread.sleep(500);
+        telemetry.addData("", "Done with beacon!");
+        telemetry.update();
+
+        driveAtPower(-.4f);
+        Thread.sleep(900);
+        pushNeitherButton();
+
+        stopDrive();
+        telemetry.addData("", "That's all for now!");
+        telemetry.update();
+
+
+        // Done.
+        telemetry.addData("Done!", "");
+
+        //  P  E  R  F  E  C  T !
+        */
+    }
+
+}
